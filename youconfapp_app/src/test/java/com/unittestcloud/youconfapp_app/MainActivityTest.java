d0d18240f9b79d37985ddca5aae8eac3fdb9523e
toniolo <davidtoniolo@gmx.de>
2013-10-03 22:14:38 +0200

    Add author.

--- youconfapp_app/src/test/java/com/unittestcloud/youconfapp_app/MainActivityTest.java
+++ youconfapp_app/src/test/java/com/unittestcloud/youconfapp_app/MainActivityTest.java
@@ -2,25 +2,24 @@ package com.unittestcloud.youconfapp_app;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
-import static org.robolectric.Robolectric.shadowOf;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.shadows.ShadowActivity;
-import org.robolectric.shadows.ShadowIntent;
 
 import com.unittestcloud.R;
-import android.content.Intent;
-import android.widget.Button;
 
+/**
+ * 
+ * @author davidtoniolo
+ *
+ */
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 
 	private MainActivity activity;
-    private Button pressMeButton;
 	
     @Before
     public void setUp() throws Exception {
@@ -30,7 +29,7 @@ public class MainActivityTest {
     @Test
     public void shouldHaveAppName() throws Exception {
         String appName = activity.getResources().getString(R.string.app_name);
-        assertThat(appName, equalTo("youconfapp_app"));
+        assertThat(appName, equalTo("youconf!app"));
     }
     
 }
