e215c39e5222f69b2e01517cc732a8697f91e3b0
toniolo <davidtoniolo@gmx.de>
2013-10-23 23:13:33 +0200

    Add menu interface and XML.

---
+++ youconfapp_menu/src/main/java/com/unittestcloud/youconfapp_menu/ActionBarMapActivityMenu.java
@@ -0,0 +1,16 @@
+package com.unittestcloud.youconfapp_menu;
+
+
+/**
+ * Serves the onClick API of the main menu in actionbar_map_menu.xml
+ * 
+ * @author davidtoniolo
+ *
+ */
+public interface ActionBarMapActivityMenu {
+
+	public void openPreferencesOnClick();
+	
+	public void refreshOnClick();
+
+}
