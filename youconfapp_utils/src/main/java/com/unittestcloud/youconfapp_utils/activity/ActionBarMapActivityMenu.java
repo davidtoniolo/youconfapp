85cada17252720dc37f6559cf9e8033b529b8ecd
d.toniolo <d.toniolo@synergetic.ag>
2013-10-23 15:36:59 +0200

    Add new interface for a new menu. Update pom.xml with localization dependency.

---
+++ youconfapp_utils/src/main/java/com/unittestcloud/youconfapp_utils/activity/ActionBarMapActivityMenu.java
@@ -0,0 +1,16 @@
+package com.unittestcloud.youconfapp_utils.activity;
+
+
+/**
+ * Serves the onClick API of the main menu in actionbar_map_menu.xml
+ * 
+ * @author davidtoniolo
+ *
+ */
+public interface ActionBarMapActivityMenu {
+
+	public void openPreferencesOnClick();
+	
+	public void refreshOnClick();
+
+}
